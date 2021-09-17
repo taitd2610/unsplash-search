5544fea06d30aa7e2d27d5ed999f3cdfa9d173d6
taitd153 <trinhdinhtai1996@gmail.com>
2021-09-17 11:20:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("code");
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const client_id = "Z2HJrW-KbZ_LouildpHux3bQodKEszsXF_Pv1VDARKs";
+  const fetchUrl = `https://api.unsplash.com/search/photos?client_id=${client_id}&query=${query}&page=${page}`;
+
+  const fetchImages = () => {
+    axios
+      .get(fetchUrl, {
+        headers: {},
+      })
+      .then((response) => {
+        setData([...data, ...response.data.results]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, [query]);
+
+  const searchImages = (e) => {
+    if (e.keyCode === 13) {
+      setQuery(e.target.value);
+      setData([]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex">
+      <input
+        type="text"
+        onKeyDown={(e) => searchImages(e)}
+        placeholder="Search For Images 🔎"
+      />
+      <InfiniteScroll
+        dataLength={data.length}
+        next={fetchImages}
+        hasMore={hasMore}
+        loader={<p>Load more...</p>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="main flex">
+          {data.map((data, key) => (
+            <div className="container" key={key}>
+              <img
+                src={data.urls.small}
+                className="image"
+                alt={data.alt_description}
+              />
+              <h4>Photo by {data.user.name} 📸</h4>
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
